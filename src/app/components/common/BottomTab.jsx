2eab87e5ed95d152b83512ff96e51f2fdd589da3
kimnerd625 <jihunkim625@yonsei.ac.kr>
2023-07-21 14:45:44 +0900

    feat: BottomTab 컴포넌트 구현중
    feat: BottomTab 컴포넌트 구현중

--- src/app/components/common/BottomTab.jsx
+++ src/app/components/common/BottomTab.jsx
@@ -3,26 +3,20 @@ import Link from "next/link";
 export default function BottomTab() {
   return (
     <footer>
-      <div>
-        <Link href="/">
-          <div>
-            <h3>
-              Home
-            </h3>
+      <div className="flex flex-row justify-around mb-6 border-t-2 py-4">
+        <Link href="/home" className="grow justify-center items-center">
+          <div className="flex-col justify-center items-center text-center space-y-1">
+            <h4 className="text-base font-medium">Home</h4>
           </div>
         </Link>
-        <Link href="/">
-          <div>
-            <h3>
-              Home
-            </h3>
+        <Link href="/care" className="grow justify-center items-center">
+          <div className="flex-col justify-center items-center text-center space-y-1">
+            <h4 className="text-base font-medium">Map</h4>
           </div>
         </Link>
-        <Link href="/">
-          <div>
-            <h3>
-              Home
-            </h3>
+        <Link href="/shop" className="grow justify-center items-center">
+          <div className="flex-col justify-center items-center text-center space-y-1">
+            <h4 className="text-base font-medium">Mypage</h4>
           </div>
         </Link>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 export default function Home() {
   return (
-    <>
-      <div>
-        <h2>
-          홈 페이지
-        </h2>
-      </div>
-    </>
+    <div className="flex grow">
+      <h2>
+        홈 페이지
+      </h2>
+    </div>
   );
 };

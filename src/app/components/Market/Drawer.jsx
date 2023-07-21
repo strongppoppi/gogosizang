489619d9b9ae952d4f94476b54b333157cf6716b
she1018 <she020108@gmail.com>
2023-07-22 03:50:57 +0900

    :bug: 드로어 내부 밀리는 현상 수정

--- src/app/components/Market/Drawer.jsx
+++ src/app/components/Market/Drawer.jsx
@@ -103,7 +103,7 @@ export default function Drawer({ children }) {
                     onTouchStart={handleTouchStart}
                     className="w-full h-8 flex justify-center"
                 >
-                    <div className="w-24 h-1 rounded-full bg-gray-200 mt-2" />
+                    <div className="w-24 h-1 rounded-full bg-gray-200 mt-2 mb-28" />
                 </div>
                 {children}
             </div>

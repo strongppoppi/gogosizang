3bd685472e7c5fc06d12eb18105326edfdc49c6d
kimnerd625 <jihunkim625@yonsei.ac.kr>
2023-07-22 01:40:53 +0900

    :lipstick: 컴포넌트 UI 수정
    :lipstick: 컴포넌트 UI 수정

--- src/app/components/Home/ShopCarousel.jsx
+++ src/app/components/Home/ShopCarousel.jsx
@@ -2,10 +2,8 @@
 
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
 
 import "swiper/css";
-import "swiper/css/pagination";
 import "./customShopSwiper.css";
 
 import defaultImage from "/public/images/defaultImage.png";
@@ -16,36 +14,32 @@ export default function ShopCarousel() {
       <Swiper
         slidesPerView={"auto"}
         spaceBetween={15}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
         mousewheel={true}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
       >
         <SwiperSlide>
-          <div className="block mb-8">
+          <div className="block">
             <Image src={defaultImage} layout="responsive" alt="기본 이미지" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="block mb-8">
+          <div className="block">
             <Image src={defaultImage} layout="responsive" alt="기본 이미지" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="block mb-8">
+          <div className="block">
             <Image src={defaultImage} layout="responsive" alt="기본 이미지" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="block mb-8">
+          <div className="block">
             <Image src={defaultImage} layout="responsive" alt="기본 이미지" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="block mb-8">
+          <div className="block">
             <Image src={defaultImage} layout="responsive" alt="기본 이미지" />
           </div>
         </SwiperSlide>

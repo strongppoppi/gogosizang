9766254af41d8184dbf87a2dd528e3d2c485d55e
kimnerd625 <jihunkim625@yonsei.ac.kr>
2023-07-22 12:06:55 +0900

    :sparkles: EntireMap 컴포넌트 네이버 지도 연동
    :sparkles: EntireMap 컴포넌트 네이버 지도 연동

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,21 +1,19 @@
-
+import EntireMap from "../components/Map/EntireMap";
 import MarketModal from "../components/Map/MarketModal";
 import MyLocationBtn from "../components/Map/MyLocationBtn";
 import NearbyMarketBtn from "../components/Map/NearbyMarketBtn";
 import SearchBar from "../components/Map/SearchBar";
 
-
 export default function MapPage() {
-
   return (
     <div className="flex-col grow overflow-y-scroll">
-      <h4>지도 페이지</h4>
-      <SearchBar/>
+      <EntireMap />
+      <SearchBar />
       <div className="w-11/12 absolute bottom-32 left-1/2 transform -translate-x-1/2 flex flex-col">
         <MarketModal marketIndex={0} />
         <div className="flex flex-row justify-between mt-4">
-          <NearbyMarketBtn/>
-          <MyLocationBtn/>
+          <NearbyMarketBtn />
+          <MyLocationBtn />
         </div>
       </div>
     </div>

d9b05ce34219a2c4570c3904bc560274bcceaff5
she1018 <she020108@gmail.com>
2023-07-24 01:19:13 +0900

    :sparkles: 선택된 마커 색상 변경하는 기능 구현

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,6 +1,7 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+import markets from "public/data/markets.json";
 import EntireMap from "../components/Map/EntireMap";
 import MarketModal from "../components/Map/MarketModal";
 import MyLocationBtn from "../components/Map/MyLocationBtn";
@@ -12,8 +13,53 @@ export default function MapPage() {
   const [naverMap, setNaverMap] = useState(null);   //네이버 지도 instance(?)
   const [markers, setMarkers] = useState({});     //현재 naverMap에 추가된 marker들
   const [selectedMarket, setSelectedMarket] = useState(null);   //선택된 시장(마커)의 key(index)
+  const [beforeMarket, setBeforeMarket] = useState(null);
   const [myCurrentLocation, setMyCurrentLocation] = useState({}); // 내 위치 정보 불러오기
   const [isSearchBarClicked, setIsSearchBarClicked] = useState(false);
+
+  // 선택한 마커 검은색으로
+  useEffect(()=> {
+    if (selectedMarket) {
+      const getClickHandler = (k) => {
+        return function () {
+            console.log("marker clicked: ", k);
+            setSelectedMarket(k);
+          }
+        };
+
+      if (beforeMarket) {
+        var newMarker = new naver.maps.Marker({
+          position: new naver.maps.LatLng(markets[beforeMarket].latitude, markets[beforeMarket].longitude),
+          map: naverMap,
+          icon: {
+              url: 'icons/marker_main.png',
+              size: new naver.maps.Size(42, 52),
+              origin: new naver.maps.Point(0, 0),
+              anchor: new naver.maps.Point(25, 26)
+          }
+        });
+        markers[beforeMarket].setMap(null);
+        naver.maps.Event.addListener(newMarker, 'click', getClickHandler(beforeMarket));
+        markers[beforeMarket] = newMarker;
+      }
+
+      var newMarker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(markets[selectedMarket].latitude, markets[selectedMarket].longitude),
+        map: naverMap,
+        icon: {
+            url: 'icons/marker_black.png',
+            size: new naver.maps.Size(42, 52),
+            origin: new naver.maps.Point(0, 0),
+            anchor: new naver.maps.Point(25, 26)
+        }
+      });
+      markers[selectedMarket].setMap(null);
+      naver.maps.Event.addListener(newMarker, 'click', getClickHandler(selectedMarket));
+      markers[selectedMarket] = newMarker;
+
+      setBeforeMarket(selectedMarket);
+    }
+  }, [selectedMarket]);
 
   const toggleSearchBar = () => {
     setIsSearchBarClicked(!isSearchBarClicked);

4805acd3471e09b45731bbc3d6b88544a446bcc6
she1018 <she020108@gmail.com>
2023-07-26 05:06:52 +0900

    :sparkles: 시장정보 영업시간 구현

--- src/app/components/Market/MarketInfo.jsx
+++ src/app/components/Market/MarketInfo.jsx
@@ -11,10 +11,12 @@ import phoneIcon from "/public/icons/phonecall.svg";
 import clockIcon from "/public/icons/clock.svg";
 import carIcon from "/public/icons/car.svg";
 import chevronDown from "/public/icons/chevron_down_small.svg";
+import chevronUp from "/public/icons/chevron_up.svg";
 
 
 export default function MarketInfo({ marketKey }) {
     const [marketApiData, setMarketApiData] = useState(null);
+    const [timeClicked, setTimeClicked] = useState(false);
 
     const apiDataRef = ref(firebaseDatabase, `market_api/${marketKey}`);
 
@@ -107,18 +109,33 @@ export default function MarketInfo({ marketKey }) {
                     </div>
                     <div className="h-12 flex flex-row items-center px-3 rounded-lg bg-gray-100 mb-3">
                         <Image src={clockIcon} width={24} height={24} alt="아이콘" />
-                        <h3 className="text-base font-normal text-black ml-2.5">영업 시간</h3>
+                        <h3 className="text-[15px] font-normal text-black ml-2.5">영업 시간</h3>
                         <div className="w-px h-6 bg-gray-400 mx-3" />
-                        <h3 className="text-base font-normal text-black mr-1">{`${openTime} - ${closeTime}`}</h3>
-                        <Image src={chevronDown} width={24} height={24} alt="아이콘" />
+                        <div onClick={() => setTimeClicked(!timeClicked)} className="flex flex-row items-center relative">
+                            <h3 className="text-[15px] font-normal text-black mr-1">{`${openTime} - ${closeTime}`}</h3>
+                            {timeClicked ?
+                                <Image src={chevronUp} width={24} height={24} alt="아이콘" /> :
+                                <Image src={chevronDown} width={24} height={24} alt="아이콘" />
+                            }
+                            {timeClicked &&
+                                <div className="absolute z-[60] bottom-0 transform -translate-x-[10px] translate-y-full flex flex-col items-center w-[120px] py-2 bg-white rounded-[10px] shadow text-[15px] text-black font-normal ">
+                                    <div>월 0:00 - 0:00</div>
+                                    <div>화 0:00 - 0:00</div>
+                                    <div>수 0:00 - 0:00</div>
+                                    <div>목 0:00 - 0:00</div>
+                                    <div>금 0:00 - 0:00</div>
+                                    <div>토 0:00 - 0:00</div>
+                                    <div>일 0:00 - 0:00</div>
+                                </div>}
+                        </div>
                         <div className="grow" />
-                        <div className="rounded px-2.5 py-1 bg-positive text-sm font-normal text-white">영업중</div>
+                        <div className="rounded px-2.5 py-1 bg-positive text-[13px] font-normal text-white">영업중</div>
                     </div>
                     <div className="h-12 flex flex-row items-center px-3 rounded-lg bg-gray-100 mb-6">
                         <Image src={carIcon} width={24} height={24} alt="아이콘" />
-                        <h3 className="text-base font-normal text-black ml-2.5">주차장</h3>
+                        <h3 className="text-[15px] font-normal text-black ml-2.5">주차장</h3>
                         <div className="w-px h-6 bg-gray-400 mx-3" />
-                        <h3 className="text-base font-normal text-black mr-1">{marketApiData.prkplceYn === "Y" ? "있음" : "없음"}</h3>
+                        <h3 className="text-[15px] font-normal text-black mr-1">{marketApiData.prkplceYn === "Y" ? "있음" : "없음"}</h3>
                     </div>
                 </div> :
                 skeleton}

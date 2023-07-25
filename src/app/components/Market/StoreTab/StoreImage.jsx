2a959f2680461e3b9292117db55a8d886172711d
she1018 <she020108@gmail.com>
2023-07-26 02:18:20 +0900

    :sparkles: 상점 데이터&이미지 불러오는 기능 구현

---
+++ src/app/components/Market/StoreTab/StoreImage.jsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { useState } from "react";
+import { ref, getDownloadURL, listAll } from "firebase/storage";
+import { firebaseStorage } from "../../../../../firebase-config";
+import Image from "next/image";
+
+import defaultImage from "/public/images/defaultImage.png";
+import arrowIcon from "public/icons/curved_arrow_white.png";
+
+
+export default function StoreImage({ marketKey, storeKey }) {
+    const [image, setImage] = useState(null);
+
+    const imageRef = ref(firebaseStorage, `stores/${marketKey}/${storeKey}`);
+
+    listAll(imageRef)
+        .then((res) => {
+            return res.items;
+        })
+        .then((list) => {
+            if (list.length === 0) {
+                setImage(defaultImage);
+            } else {
+                const itemRef = list[0];
+                getDownloadURL(itemRef)
+                    .then((url) => {
+                        setImage(url);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    // 로딩 중 보여질 UI
+    var skeleton = (
+        <div className="w-11/12 h-52 rounded-lg overflow-hidden relative mb-2.5 bg-gray-200 animate-pulse" />
+    );
+
+    return (
+        image ?
+            <div className="w-11/12 h-52 rounded-lg overflow-hidden relative mb-2.5" >
+                <Image
+                    src={image}
+                    placeholder="blur"
+                    blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
+                    fill={true}
+                    className="object-cover"
+                    alt="상점 이미지" />
+                <div className="w-11 h-11 rounded-lg bg-main absolute right-2.5 bottom-2.5 flex justify-center items-center">
+                    <Image src={arrowIcon} width={24} height={24} alt="아이콘" />
+                </div>
+            </div> :
+            skeleton
+    )
+}
--- src/app/components/Market/StoreTab/StoreItem.jsx
+++ src/app/components/Market/StoreTab/StoreItem.jsx
@@ -1,41 +1,63 @@
+import { useState, useEffect } from "react";
+import { firebaseDatabase, firebaseStorage } from "../../../../../firebase-config";
+import { ref, get } from "firebase/database";
+
+
 import Image from "next/image";
 
 import defaultImage from "public/images/defaultImage.png";
-import arrowIcon from "public/icons/curved_arrow_white.svg";
+import arrowIcon from "public/icons/curved_arrow_white.png";
+import StoreImage from "./StoreImage";
 
 export default function StoreItem({ marketKey, storeKey }) {
+    const [storeData, setStoreData] = useState(null);
+
+    const storeRef = ref(firebaseDatabase, `stores/${marketKey}/${storeKey}`);
+
+    useEffect(() => {
+        // database
+        if (!storeData) {
+            get(storeRef).then((snapshot) => {
+                if (snapshot.exists()) {
+                    setStoreData(snapshot.val());
+                } else {
+                    console.log("No data available");
+                }
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+    }, []);
 
     var skeleton = (
-        <div className="w-full flex flex-col justify-start items-center my-4">
-            <div className="w-11/12 h-52 rounded-lg overflow-hidden relative mb-2.5 bg-gray-100" />
+        <div className="w-full flex flex-col justify-start items-center my-4 animate-pulse">
+            <div className="w-11/12 h-52 rounded-lg overflow-hidden relative mb-2.5 bg-gray-200" />
             <div className="w-11/12 flex flex-row justify-start items-center mb-2.5">
-                <div className="w-48 h-7 rounded-lg bg-gray-100" />
+                <div className="w-48 h-7 rounded-lg bg-gray-200" />
             </div>
             <div className="w-11/12 flex flex-row justify-start items-center">
-                <div className="w-14 h-8 rounded-lg mr-2 bg-gray-100" />
-                <div className="w-20 h-8 rounded-lg mr-2 bg-gray-100" />
-                <div className="w-16 h-8 rounded-lg bg-gray-100" />
+                <div className="w-14 h-8 rounded-lg mr-2 bg-gray-200" />
+                <div className="w-20 h-8 rounded-lg mr-2 bg-gray-200" />
+                <div className="w-16 h-8 rounded-lg bg-gray-200" />
             </div>
         </div>
     );
 
+
     return (
-        <div className="w-full flex flex-col justify-start items-center my-4">
-            <div className="w-11/12 h-52 rounded-lg overflow-hidden relative mb-2.5">
-                <Image src={defaultImage} fill={true} className="object-cover" alt="상점 사진" />
-                <div className="w-11 h-11 rounded-lg bg-main absolute right-2.5 bottom-2.5 flex justify-center items-center">
-                    <Image src={arrowIcon} width={24} height={24} alt="아이콘" />
+        storeData ?
+            <div className="w-full flex flex-col justify-start items-center my-4">
+                <StoreImage marketKey={marketKey} storeKey={storeKey} />
+                <div className="w-11/12 flex flex-row justify-start items-center mb-2.5">
+                    <h3 className="text-xl font-medium text-black mr-2">{storeData["점포명"]}</h3>
+                    <h3 className="text-xs font-normal text-gray-600">{storeData["취급품목"]}</h3>
                 </div>
-            </div>
-            <div className="w-11/12 flex flex-row justify-start items-center mb-2.5">
-                <h3 className="text-xl font-medium text-black mr-2">우리 농산물</h3>
-                <h3 className="text-xs font-normal text-gray-600">야채·과일</h3>
-            </div>
-            <div className="w-11/12 flex flex-row justify-start items-center">
-                <Tag>맛있어요</Tag>
-                <Tag>가성비 좋아요</Tag>
-            </div>
-        </div>
+                <div className="w-11/12 flex flex-row justify-start items-center">
+                    <Tag>맛있어요</Tag>
+                    <Tag>가성비 좋아요</Tag>
+                </div>
+            </div > :
+            skeleton
     );
 }
 

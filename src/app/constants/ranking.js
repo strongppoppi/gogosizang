dfc56b6a878e2aa29680c998c24bb38b7db6e769
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-08-06 05:42:29 +0900

    :sparkles: 평점 등록 함수 구현
    :sparkles: 평점 등록 함수 구현

--- src/app/constants/ranking.js
+++ src/app/constants/ranking.js
@@ -1,15 +1,47 @@
 import { firebaseDatabase } from "../../../firebase-config";
-import { ref, get } from "firebase/database";
-
-const rankingDataRef = ref(firebaseDatabase, `rankings`);
+import { ref, set, onValue, get } from "firebase/database";
 
 // 점포 평점 입력하기
-export function addRankingData() {}
+export function addRankingData({
+  marketKey,
+  storeKey,
+  rating,
+  marketName,
+  storeName,
+}) {
+  const rankingRef = ref(firebaseDatabase, `rankings/${marketKey}-${storeKey}`);
+  get(rankingRef).then((snapshot) => {
+    if (snapshot.exists()) {
+      const existingData = snapshot.val();
+      const newTotalStars = existingData.totalStars + rating;
+      const newCount = existingData.count + 1;
+
+      set(rankingRef, {
+        totalStars: newTotalStars,
+        count: newCount,
+        marketName: marketName,
+        storeName: storeName,
+      }).then(() => {
+        console.log("평점 갱신 완료");
+      });
+    } else {
+      // 기존 데이터가 없을 경우 새로 생성
+      set(rankingRef, {
+        totalStars: rating,
+        count: 1,
+        marketName: marketName,
+        storeName: storeName,
+      }).then(() => {
+        console.log("신규 평점 등록 완료");
+      });
+    }
+  });
+}
 
 // 점포 평점 데이터 가져오기
 export function fetchRankingData({ rankingData, setRankingData }) {
   if (rankingData.length === 0) {
-    get(rankingDataRef)
+    get(ref(firebaseDatabase, `rankings`))
       .then((data) => {
         if (data.exists()) {
           const rankingData = data.val();

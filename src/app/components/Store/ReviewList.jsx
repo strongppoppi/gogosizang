ea556f39c30a919ceb541511fe40a3b9a397b321
she1018 <she020108@gmail.com>
2023-08-10 19:13:34 +0900

    :sparkles: 리뷰목록에 평균 별점, 리뷰 수 표시

--- src/app/components/Store/ReviewList.jsx
+++ src/app/components/Store/ReviewList.jsx
@@ -9,9 +9,12 @@ import ReviewItem from "./ReviewItem";
 export default function ReviewList({ marketKey, storeKey }) {
     const [reviews, setReviews] = useState(null);
     const [tags, setTags] = useState(null);
+    const [rating, setRating] = useState(null);
+    const [count, setCount] = useState(0);
 
     const reviewRef = ref(firebaseDatabase, `reviews/${marketKey}/${storeKey}`);
     const tagRef = ref(firebaseDatabase, `tags/${marketKey}/${storeKey}`);
+    const ratingRef = ref(firebaseDatabase, `rankings/${marketKey}-${storeKey}`);
 
     const tagEmoji = {
         카드가능: "카드가능💳",
@@ -36,7 +39,7 @@ export default function ReviewList({ marketKey, storeKey }) {
                 }
             );
         }
-    }, []);
+    }, [reviews]);
 
     useEffect(() => {
         if (!tags) {
@@ -59,16 +62,36 @@ export default function ReviewList({ marketKey, storeKey }) {
         }
     }, [tags]);
 
+    useEffect(() => {
+        if (!rating) {
+            onValue(ratingRef,
+                (snapshot) => {
+                    if (snapshot.exists()) {
+                        const data = snapshot.val();
+                        console.log(data);
+                        setRating(parseFloat((data.totalStars / data.count).toFixed(1)));
+                        setCount(data.count);
+                    } else {
+                        setRating(" - ");
+                    }
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+        }
+    }, [rating]);
+
     return (
         <div className="w-full flex flex-col items-center">
             <div className="w-full h-[10px] bg-gray-200" />
             <div className="w-full flex flex-row justify-start items-center px-5 pt-6 pb-2.5">
                 <h3 className="text-[17px] font-medium text-black mr-2.5">평점</h3>
                 <Image src={starIcon} width={24} height={24} alt="아이콘" className="mb-0.5" />
-                <h3 className="text-[15px] font-normal text-black leading-6">4.4</h3>
+                <h3 className="text-[15px] font-normal text-black leading-6">{rating}</h3>
             </div>
-            <h3 className="w-full text-[17px] font-medium text-black px-5 pb-5">리뷰 (8)</h3>
-            <div className="flex flex-row justify-start items-start flex-wrap gap-x-3 gap-y-[9px] px-5 mb-3">
+            <h3 className="w-full text-[17px] font-medium text-black px-5 pb-5">리뷰 ({count})</h3>
+            <div className="w-full flex flex-row justify-start items-start flex-wrap gap-x-3 gap-y-[9px] px-5 mb-3">
                 {tags && tags.map((tag, index) => (
                     <div
                         key={index}

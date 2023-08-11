31c63e90c961b23b3c1720d00d1dd0bb2a730fa3
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-08-11 22:33:39 +0900

    :lipstick: 탭바 UI 수정
    :lipstick: 탭바 UI 수정

--- src/app/components/common/BottomTab.jsx
+++ src/app/components/common/BottomTab.jsx
@@ -16,14 +16,15 @@ export default function BottomTab() {
   const pathName = usePathname();
 
   return (
-    <div className="absolute bottom-0 left-0 w-full bg-white z-20">
+    <div className="absolute bottom-[-90px] left-0 w-full bg-white z-20 h-[180px]">
       <footer>
         <div className="flex flex-row justify-around border-t-2 py-4">
           <Link href="/" className="grow justify-center items-center">
             <div className="flex-col justify-center items-center text-center space-y-1">
               <div
-                className={`${pathName === "/" ? "bg-main" : "bg-white"
-                  } px-4 py-0.5 inline-flex align-middle rounded-lg`}
+                className={`${
+                  pathName === "/" ? "bg-main" : "bg-white"
+                } px-4 py-0.5 inline-flex align-middle rounded-lg`}
               >
                 <Image
                   src={pathName === "/" ? home_white : home_grey}
@@ -34,8 +35,9 @@ export default function BottomTab() {
                 />
               </div>
               <h4
-                className={`${pathName === "/" ? "text-black" : "text-gray-500"
-                  } text-base font-regular tracking-tight`}
+                className={`${
+                  pathName === "/" ? "text-black" : "text-gray-500"
+                } text-base font-regular tracking-tight`}
               >
                 홈
               </h4>
@@ -44,8 +46,9 @@ export default function BottomTab() {
           <Link href="/map" className="grow justify-center items-center">
             <div className="flex-col justify-center items-center text-center space-y-1">
               <div
-                className={`${pathName === "/map" ? "bg-main" : "bg-white"
-                  } px-4 py-0.5 inline-flex align-middle rounded-lg`}
+                className={`${
+                  pathName === "/map" ? "bg-main" : "bg-white"
+                } px-4 py-0.5 inline-flex align-middle rounded-lg`}
               >
                 <Image
                   src={pathName === "/map" ? map_white : map_grey}
@@ -56,8 +59,9 @@ export default function BottomTab() {
                 />
               </div>
               <h4
-                className={`${pathName === "/map" ? "text-black" : "text-gray-500"
-                  } text-base font-regular tracking-tight`}
+                className={`${
+                  pathName === "/map" ? "text-black" : "text-gray-500"
+                } text-base font-regular tracking-tight`}
               >
                 시장찾기
               </h4>
@@ -66,8 +70,9 @@ export default function BottomTab() {
           <Link href="/mypage" className="grow justify-center items-center">
             <div className="flex-col justify-center items-center text-center space-y-1">
               <div
-                className={`${pathName === "/mypage" ? "bg-main" : "bg-white"
-                  } px-4 py-0.5 inline-flex align-middle rounded-lg`}
+                className={`${
+                  pathName === "/mypage" ? "bg-main" : "bg-white"
+                } px-4 py-0.5 inline-flex align-middle rounded-lg`}
               >
                 <Image
                   src={pathName === "/mypage" ? setting_white : setting_grey}
@@ -78,8 +83,9 @@ export default function BottomTab() {
                 />
               </div>
               <h4
-                className={`${pathName === "/mypage" ? "text-black" : "text-gray-500"
-                  } text-base font-regular tracking-tight`}
+                className={`${
+                  pathName === "/mypage" ? "text-black" : "text-gray-500"
+                } text-base font-regular tracking-tight`}
               >
                 설정
               </h4>

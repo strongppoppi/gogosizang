24e908d0fac1731c3ddfef197c955b95e7cfd87c
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-08-12 16:30:20 +0900

    :sparkles: 랭킹 점수 소수점 자리 처리 및 큐레이션 네비게이션 버튼 추가
    :sparkles: 랭킹 점수 소수점 자리 처리 및 큐레이션 네비게이션 버튼 추가

--- src/app/components/Editor/CurationItem.jsx
+++ src/app/components/Editor/CurationItem.jsx
@@ -2,11 +2,13 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import { getStoreInfo } from "@/app/constants/storeInfo";
 
 import marketIcon from "/public/icons/market_main.png";
 import markerIcon from "/public/icons/marker_grey.png";
+import nextButtonImage from "/public/icons/curved_arrow_white.png";
 
 export default function CurationItem({ storeId, imageOne, imageTwo, content }) {
   const [storeData, setStoreData] = useState(null);
@@ -26,20 +28,35 @@ export default function CurationItem({ storeId, imageOne, imageTwo, content }) {
       <div className="w-[360px] h-px bg-gray-200 mb-4" />
       {storeData && (
         <>
-          <div className="flex flex-row items-center space-x-2 mb-1.5">
-            <Image src={marketIcon} width={24} height={24} alt="아이콘" />
-            <h1 className="text-lg font-medium text-black">
-              {storeData.점포명}
-            </h1>
-            <h5 className="text-[13px] font-normal text-gray-500">
-              {storeData.분류}
-            </h5>
-          </div>
-          <div className="flex flex-row items-center space-x-2 mb-5">
-            <Image src={markerIcon} alt="아이콘" width={24} height={24} />
-            <h4 className="text-[15px] font-normal text-gray-600 ml-0.5">
-              {storeData.주소}
-            </h4>
+          <div className="flex flex-row justify-between items-center mb-4">
+            <div className="flex flex-col justify-start">
+              <div className="flex flex-row items-center space-x-2 mb-1.5">
+                <Image src={marketIcon} width={24} height={24} alt="아이콘" />
+                <h1 className="text-lg font-medium text-black">
+                  {storeData.점포명}
+                </h1>
+                <h5 className="text-[13px] font-normal text-gray-500">
+                  {storeData.분류}
+                </h5>
+              </div>
+              <div className="flex flex-row items-center space-x-2">
+                <Image src={markerIcon} alt="아이콘" width={24} height={24} />
+                <h4 className="text-[15px] font-normal text-gray-600 ml-0.5">
+                  {storeData.주소}
+                </h4>
+              </div>
+            </div>
+            <Link href={`/market/800`}>
+              <div className="w-[44px] h-[44px] bg-main rounded-lg overflow-hidden flex flex-row justify-center items-center drop-shadow-sm">
+                <Image
+                  src={nextButtonImage}
+                  width={24}
+                  height={24}
+                  alt="다음 버튼"
+                  className="object-contain"
+                />
+              </div>
+            </Link>
           </div>
         </>
       )}
